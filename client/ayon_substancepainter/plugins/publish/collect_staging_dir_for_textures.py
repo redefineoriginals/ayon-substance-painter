c3f471d032bfcdea2bb634d62050b7920a347e80
Kayla Man <moonyuet2008@hotmail.com>
2024-12-20 15:45:20 +0800

    includes the textures and images into the families of the staging dir collector

--- client/ayon_substancepainter/plugins/publish/collect_staging_dir_for_textures.py
+++ client/ayon_substancepainter/plugins/publish/collect_staging_dir_for_textures.py
@@ -1,8 +1,9 @@
+import os
 import copy
 
 import pyblish.api
 from ayon_core.pipeline import publish
-
+from ayon_substancepainter.api.lib import get_parsed_export_maps
 
 class CollectStagingDirExportConfig(pyblish.api.InstancePlugin):
     """Collect Staging Dir for Export Config to export the textures"""
@@ -10,14 +11,14 @@ class CollectStagingDirExportConfig(pyblish.api.InstancePlugin):
 
     label = "Collect Staging dir for Export Config"
     hosts = ["substancepainter"]
-    families = ["textureSet"]
+    families = ["textureSet", "textures", "image"]
     order = pyblish.api.CollectorOrder + 0.491
 
     def process(self, instance):
         export_config = copy.deepcopy(instance.data["exportConfig"])
         export_config["exportPath"] = publish.get_instance_staging_dir(instance)
         instance.data["exportConfig"] = export_config
-
+        instance.data.update(instance.data["exportConfig"])
 
 class CollectStagingDirTexture(pyblish.api.InstancePlugin):
     """Collect Staging Dir as the representation data for the texture publish"""
@@ -25,14 +26,15 @@ class CollectStagingDirTexture(pyblish.api.InstancePlugin):
 
     label = "Collect Staging dir for texture"
     hosts = ["substancepainter"]
-    families = ["image"]
+    families = ["image", "textures"]
     order = pyblish.api.CollectorOrder + 0.4911
 
     def process(self, instance):
         representations: "list[dict]" = instance.data["representations"]
+        staging_dir = instance.data["exportConfig"]["exportPath"]
         updated_representations = []
         for representation in list(representations):
             tmp_representation = copy.deepcopy(representation)
-            tmp_representation["stagingDir"] = instance.data["exportConfig"]["exportPath"]
+            tmp_representation["stagingDir"] = staging_dir
             updated_representations.append(tmp_representation)
         instance.data["representations"] = updated_representations
